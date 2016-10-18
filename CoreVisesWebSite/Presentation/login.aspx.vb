f4d163bc61e506d13b2f43718497c90973d1563b
Yunen Ramos Ramírez <yunen.ramos@ucr.ac.cr>
2016-10-17 19:30:36 -0600

    Welcome ya muestra el nombre completo

--- CoreVisesWebSite/Presentation/login.aspx.vb
+++ CoreVisesWebSite/Presentation/login.aspx.vb
@@ -13,12 +13,15 @@
 
     Protected Sub btnLogIn_Click(sender As Object, e As EventArgs)
         Dim service As New ClientServiceReference.ClientServiceClient
+        Dim clientTemp As New ClientServiceReference.Client
         Dim tempValue As Integer
         tempValue = service.verifyExistsClient(txtNameUser.Text, txtPassword.Text)
 
         If (tempValue <> 0) Then
             Session.Item("condition") = 1
+            clientTemp = service.getClient(txtNameUser.Text)
             Session.Add("user", txtNameUser.Text)
+            Session.Add("name", (clientTemp.Name + " " + clientTemp.LastName_1 + " " + clientTemp.LastName_2))
             wrongMessage.Style.Add("display", "none")
             MyClass.Response.Redirect("~/Presentation/welcome.aspx")
         Else
--- CoreVisesWebSite/Presentation/welcome.aspx.vb
+++ CoreVisesWebSite/Presentation/welcome.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        lblName.Text = CType(Session("user"), String)
+        lblName.Text = CType(Session("name"), String)
     End Sub
 
 End Class
